e647afb9665b2947d6fc1453712d227f13df8687
Manak <manak.kapoor@Gmail.com>
2021-10-31 22:39:22 +0800

    Added fallbacks for all variables in axios log

--- dist/lib/axios.js
+++ dist/lib/axios.js
@@ -13,22 +13,22 @@ instance.interceptors.request.use(function (config) {
     return Promise.reject(error);
 });
 instance.interceptors.response.use((res) => {
-    var _a, _b;
-    let status = res.status || "NO_STATUS_CODE";
-    let method = res.request.method.toUpperCase();
-    let url = res.config.url;
-    let startTime = parseInt(((_b = (_a = res.config) === null || _a === void 0 ? void 0 : _a.headers) === null || _b === void 0 ? void 0 : _b.rqStartTime) || "0");
+    var _a, _b, _c, _d, _e;
+    let status = (res === null || res === void 0 ? void 0 : res.status) || "NO_STATUS_CODE";
+    let method = ((_b = (_a = res === null || res === void 0 ? void 0 : res.request) === null || _a === void 0 ? void 0 : _a.method) === null || _b === void 0 ? void 0 : _b.toUpperCase()) || "NO_METHOD";
+    let url = ((_c = res === null || res === void 0 ? void 0 : res.config) === null || _c === void 0 ? void 0 : _c.url) || "NO_URL";
+    let startTime = parseInt(((_e = (_d = res.config) === null || _d === void 0 ? void 0 : _d.headers) === null || _e === void 0 ? void 0 : _e.rqStartTime) || "0");
     let time = startTime != 0 ? new Date().getTime() - startTime : "Time Error";
     index_1.log.info(`[AXIOS] ${status} ${method} ${url} ${time}ms`);
     return res;
 }, (res) => {
-    var _a, _b, _c;
+    var _a, _b, _c, _d, _e, _f;
     let response = res.response || {};
     let status = response.status || "NO_STATUS_CODE";
-    let method = res.config.method.toUpperCase();
-    let url = res.config.url;
-    let body = ((_a = res.request) === null || _a === void 0 ? void 0 : _a.body) || {};
-    let startTime = parseInt(((_c = (_b = res.config) === null || _b === void 0 ? void 0 : _b.headers) === null || _c === void 0 ? void 0 : _c.rqStartTime) || "0");
+    let method = ((_b = (_a = res === null || res === void 0 ? void 0 : res.request) === null || _a === void 0 ? void 0 : _a.method) === null || _b === void 0 ? void 0 : _b.toUpperCase()) || "NO_METHOD";
+    let url = ((_c = res === null || res === void 0 ? void 0 : res.config) === null || _c === void 0 ? void 0 : _c.url) || "NO_URL";
+    let body = ((_d = res.request) === null || _d === void 0 ? void 0 : _d.body) || {};
+    let startTime = parseInt(((_f = (_e = res.config) === null || _e === void 0 ? void 0 : _e.headers) === null || _f === void 0 ? void 0 : _f.rqStartTime) || "0");
     let time = startTime != 0 ? new Date().getTime() - startTime : "Time Error";
     index_1.log.error(`[AXIOS] ${status} ${method} ${url} ${time}ms\nRequest Body`, body);
     return res;
--- lib/axios.ts
+++ lib/axios.ts
@@ -14,9 +14,9 @@ instance.interceptors.request.use(function (config) {
 });
 
 instance.interceptors.response.use((res) => {
-  let status = res.status || "NO_STATUS_CODE";
-  let method = res.request.method.toUpperCase();
-  let url = res.config.url;
+  let status = res?.status || "NO_STATUS_CODE";
+  let method = res?.request?.method?.toUpperCase() || "NO_METHOD";
+  let url = res?.config?.url || "NO_URL";
   let startTime = parseInt(res.config?.headers?.rqStartTime || "0");
   let time = startTime != 0 ? new Date().getTime() - startTime : "Time Error";
   log.info(`[AXIOS] ${status} ${method} ${url} ${time}ms`);
@@ -24,8 +24,8 @@ instance.interceptors.response.use((res) => {
 }, (res) => {
   let response = res.response || {};
   let status = response.status || "NO_STATUS_CODE";
-  let method = res.config.method.toUpperCase();
-  let url = res.config.url;
+  let method = res?.request?.method?.toUpperCase() || "NO_METHOD";
+  let url = res?.config?.url || "NO_URL";
   let body = res.request?.body || {};
   let startTime = parseInt(res.config?.headers?.rqStartTime || "0");
   let time = startTime != 0 ? new Date().getTime() - startTime : "Time Error";
